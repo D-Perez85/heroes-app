3322ae22ab4d22b96a78e119a17d68853c5183ae
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-03-13 21:57:01 -0300

    Agregado Metodo handleLogOut

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+    
+  const navigate =  useNavigate();
+  const handleLogout = () => {
+    navigate('/login', {
+      replace: true
+    });
+  };
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <Link className="navbar-brand" to="/"></Link>
@@ -20,8 +27,8 @@ export const Navbar = () => {
       </div>
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          {/* <span className="nav-item nav-link text-info">Damian</span> */}
-          <button className="nav-item nav-link btn">Logout</button>
+          <span className="nav-item nav-link text-info">Damian</span>
+          <button className="nav-item nav-link btn" onClick={handleLogout}>LOGOUT</button>
         </ul>
       </div>
     </nav>
