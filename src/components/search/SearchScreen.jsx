36f649f9d4a42d292bbf5d24cf445168db880511
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-03-15 17:25:41 -0300

    Hook Use Form + SearchScreen component

--- src/components/search/SearchScreen.jsx
+++ src/components/search/SearchScreen.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "../../hooks/useForm";
 
 const Search = () => {
+  const[formValues, handleChange, reset] = useForm({
+    searchText: '',
+  }); 
+  const {searchText} = formValues; 
+
+  const handleSubmit = (e) =>{
+    console.log(searchText);
+    e.preventDefault(); 
+    reset(); 
+  }
+
   return (
-    <div>
-        <h1>Search</h1>
-    </div>
-  )
-}
-export default Search; 
+    <>
+      <h1>Busquedas</h1>
+      <hr />
+      <div className="row">
+        <div className="col-5">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Buscar un heroe"
+              className="form-control"
+              name="searchText"
+              value={searchText}
+              onChange={handleChange}
+              autoComplete="off"
+            />
+            <button type="submit" className="btn btn-outline-primary mt-1">
+              SUBMIT
+            </button>
+          </form>
+        </div>
+        <div className="col-7">
+          <h4>Resultados</h4>
+          <hr />
+
+          {/* {
+            (q === '') ? <div className='alert alert-info'>
+                Buscar un heroe
+            </div> : (heroFiltered.length === 0) && <div className='alert alert-danger'>No hay resultados para: {q}</div>
+          } */}
+
+          {/* {heroFiltered.map((value)=>{
+            return <HeroCard key={value.id} {...value}/>
+          })} */}
+        </div>
+      </div>
+    </>
+  );
+};
+export default Search;
