7f3109532775bfb5a70e7cab9288f23b450d92c6
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-03-15 16:30:33 -0300

    useMemo para memorizar el estado del componente

--- src/components/hero/HeroList.jsx
+++ src/components/hero/HeroList.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React, {useMemo} from "react";
 import { getHeroesByPublisher } from "../../helpers/getHeroesByPublisher";
 import HeroCard from "./HeroCard";
 
 const HeroList = ({ publisher }) => {
-  const heroes = getHeroesByPublisher(publisher);
+  const heroes = useMemo(()=> getHeroesByPublisher(publisher), [publisher]); 
   return (
+    <>
     <div className="row row-cols-1 row-cols-md-3 g-3 animate__animated animate__fadeInUp">      
         {heroes.map((value) => {
           return <HeroCard key={value.id} {...value}/>
         })}
     </div>
+    </>
   );
 };
 export default HeroList;
