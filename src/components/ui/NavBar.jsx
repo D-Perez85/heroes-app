9ee1f97af38fcb20c2947487900e100928d47ca9
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-03-18 14:31:46 -0300

    Context + Reducer de User

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/authContext";
 
 export const Navbar = () => {
+  const {user} = useContext(AuthContext); 
     
   const navigate =  useNavigate();
   const handleLogout = () => {
@@ -27,7 +29,7 @@ export const Navbar = () => {
       </div>
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-info">User</span>
+          <span className="nav-item nav-link text-info">{user.name}</span>
           <button className="nav-item nav-link btn" onClick={handleLogout}>LOGOUT</button>
         </ul>
       </div>
