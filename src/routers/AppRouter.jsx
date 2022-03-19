c454200994de1afe6062aa021bc2d5c5ae0a0a76
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-03-19 12:12:55 -0300

    Public Route

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -3,20 +3,23 @@ import {BrowserRouter as Router, Routes, Route  } from "react-router-dom";
 import Login from '../components/login/LoginScreen';
 import DashboardRouter from './DashboardRouter';
 import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 const AppRouter = () => {
   return (
       <Router>
           <div>
               <Routes>
-                  <Route path="/login" element={<Login />} />
-                  <Route path="*" element={
-                  <PrivateRoute>
-                      <DashboardRouter/>
-                  </PrivateRoute>
+                  <Route path="/login" element={
+                      <PublicRoute>
+                          <Login />
+                      </PublicRoute>
                   } />
-
-                  {/* <Route path="*" element={<DashboardRouter/>} /> */}
+                  <Route path="*" element={
+                    <PrivateRoute>
+                        <DashboardRouter/>
+                    </PrivateRoute>
+                  } />
               </Routes>
           </div>
       </Router>
