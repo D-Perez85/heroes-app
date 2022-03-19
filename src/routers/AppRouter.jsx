2751e35b67ece87dd835f1695e0235d47682b6d0
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-03-19 11:57:06 -0300

    Private Route

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router, Routes, Route  } from "react-router-dom";
 import Login from '../components/login/LoginScreen';
 import DashboardRouter from './DashboardRouter';
+import PrivateRoute from './PrivateRoute';
 
 const AppRouter = () => {
   return (
@@ -9,7 +10,13 @@ const AppRouter = () => {
           <div>
               <Routes>
                   <Route path="/login" element={<Login />} />
-                  <Route path="*" element={<DashboardRouter/>} />
+                  <Route path="*" element={
+                  <PrivateRoute>
+                      <DashboardRouter/>
+                  </PrivateRoute>
+                  } />
+
+                  {/* <Route path="*" element={<DashboardRouter/>} /> */}
               </Routes>
           </div>
       </Router>
